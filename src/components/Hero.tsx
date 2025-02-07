8b7e26db15f30c755028de445c9c1749b6b94bbf
lucasde almeida <lucasdealmeidadev@gmail.com>
2025-02-07 00:08:26 -0300

    traduçãod e textos

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,16 @@
 import "../styles/global.css";
 import WhatsAppButton from "./WhatsAppButton";
+import { useLanguage } from "../context/useLanguage";
 
 const Hero = () => {
+  const { translations } = useLanguage();
+
   return (
     <section id="hero" className="hero">
       <div className="hero-content">
-        <h1 className="hero-content-title">Filling </h1>
-        <span>several containers every year</span>
-        <WhatsAppButton text="" />
+        <h1 className="hero-content-title">{translations.hero_title}</h1>
+        <span>{translations.hero_subtitle}</span>
+        <WhatsAppButton  text="" />
       </div>
     </section>
   );
